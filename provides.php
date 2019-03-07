b47e724183bb2f6ec2a4bf70d0ac0e8fda4360c8
ofirgalcon <ofir@gal.co.uk>
2019-03-07 22:39:53 +0000

    Update provides.php

--- provides.php
+++ provides.php
@@ -4,4 +4,9 @@ return array(
     'client_tabs' => array(
         'teamviewer-tab' => array('view' => 'teamviewer_tab', 'i18n' => 'teamviewer.teamviewer'),
     ),
+    'listings' => array(
+        'teamviewer' => array('view' => 'teamviewer_listing', 'i18n' => 'teamviewer.teamviewer'),
+    ),
 );
+
+
