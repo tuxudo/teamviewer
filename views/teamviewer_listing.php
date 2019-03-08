0e5ce7ea90c4eb79f4e13b46fcafa05f387b10ad
ofirgalcon <ofir@gal.co.uk>
2019-03-08 10:45:37 +0000

    Create teamviewer_listing.php (#1)
    * Create teamviewer_listing.php
    
    * Fixed title

---
+++ views/teamviewer_listing.php
@@ -0,0 +1,82 @@
+<?php $this->view('partials/head'); ?>
+
+<?php //Initialize models needed for the table
+new Machine_model;
+new Reportdata_model;
+new Teamviewer_model;
+?>
+
+<div class="container">
+
+  <div class="row">
+
+  	<div class="col-lg-12">
+
+		  <h3><span data-i18n="teamviewer.reporttitle"></span> <span id="total-count" class='label label-primary'>…</span></h3>
+		  <table class="table table-striped table-condensed table-bordered">
+		    <thead>
+		      <tr>
+		      	<th data-i18n="listing.computername" data-colname='machine.computer_name'></th>
+		        <th data-i18n="serial" data-colname='reportdata.serial_number'></th>
+		        <th data-i18n="username" data-colname='reportdata.long_username'></th>
+		        <th data-i18n="teamviewer.clientid" data-colname='teamviewer.clientid'></th>	        
+		        <th data-i18n="teamviewer.always_online" data-colname='teamviewer.always_online'></th>	        
+		        <th data-i18n="teamviewer.version" data-colname='teamviewer.version'></th>	        
+		      </tr>
+		    </thead>
+		    <tbody>
+		    	<tr>
+					<td data-i18n="listing.loading" colspan="3" class="dataTables_empty"></td>
+				</tr>
+		    </tbody>
+		  </table>
+    </div> <!-- /span 12 -->
+  </div> <!-- /row -->
+</div>  <!-- /container -->
+
+<script type="text/javascript">
+
+	$(document).on('appUpdate', function(e){
+
+		var oTable = $('.table').DataTable();
+		oTable.ajax.reload();
+		return;
+
+	});
+
+	$(document).on('appReady', function(e, lang) {
+		// Get column names from data attribute
+		var columnDefs = [],
+            col = 0; // Column counter
+		$('.table th').map(function(){
+              columnDefs.push({name: $(this).data('colname'), targets: col});
+              col++;
+		});
+	    oTable = $('.table').dataTable( {
+	        columnDefs: columnDefs,
+	        ajax: {
+                url: appUrl + '/datatables/data',
+                type: "POST"
+            },
+            dom: mr.dt.buttonDom,
+            buttons: mr.dt.buttons,
+	        createdRow: function( nRow, aData, iDataIndex ) {
+	        	// Update name in first column to link
+	        	var name=$('td:eq(0)', nRow).html();
+	        	if(name == ''){name = "No Name"};
+	        	var sn=$('td:eq(1)', nRow).html();
+	        	var link = mr.getClientDetailLink(name, sn);
+	        	$('td:eq(0)', nRow).html(link);
+	        	
+	        	// Status
+	        	var always_online=$('td:eq(4)', nRow).html();
+	        	always_online = always_online == 1 ? '<span class="label label-success">'+i18n.t('enabled')+'</span>' :
+	        	(always_online === '0' ? '<span class="label label-danger">'+i18n.t('disabled')+'</span>' : '')
+	        	$('td:eq(4)', nRow).html(always_online)
+                
+	        }
+	    });
+	});
+</script>
+
+<?php $this->view('partials/foot'); ?>
