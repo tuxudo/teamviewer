59c6a6c82166c27e974fb37974df3ad82037e7c0
tuxudo <tvxudo@gmail.com>
2019-03-08 06:30:45 -0500

    Tweaked client script, finished listing

--- teamviewer_model.php
+++ teamviewer_model.php
@@ -4,28 +4,27 @@ use CFPropertyList\CFPropertyList;
 
 class Teamviewer_model extends \Model 
 {
-
     public function __construct($serial='')
     {
         parent::__construct('id', 'teamviewer'); //primary key, tablename
         $this->rs['id'] = 0;
         $this->rs['serial_number'] = $serial;
-        $this->rs['always_online'] = 0;
+        $this->rs['always_online'] = 0; // True/False
         $this->rs['autoupdatemode'] = 0;
         $this->rs['clientid'] = 0;
         $this->rs['clientic'] = 0;
-        $this->rs['had_a_commercial_connection'] = 0;
+        $this->rs['had_a_commercial_connection'] = 0; // True/False
         $this->rs['ipc_port_service'] = 0;
         $this->rs['lastmacused'] = '';
         $this->rs['licensetype'] = 0;
         $this->rs['midversion'] = 0;
-        $this->rs['moverestriction'] = 0;
-        $this->rs['security_adminrights'] = 0;
+        $this->rs['moverestriction'] = 0; // True/False
+        $this->rs['security_adminrights'] = 0; // True/False
         $this->rs['security_passwordstrength'] = 0;
         $this->rs['version'] = '';
-        $this->rs['update_available'] = 0;
-        $this->rs['is_not_first_run_without_connection'] = 0;
-        $this->rs['is_not_running_test_connection'] = 0;
+        $this->rs['update_available'] = 0; // True/False
+        $this->rs['is_not_first_run_without_connection'] = 0; // True/False
+        $this->rs['is_not_running_test_connection'] = 0; // True/False
         $this->rs['meeting_username'] = '';
         $this->rs['prefpath'] = '';
         $this->rs['updateversion'] = '';
--- views/teamviewer_listing.php
+++ views/teamviewer_listing.php
@@ -7,11 +7,8 @@ new Teamviewer_model;
 ?>
 
 <div class="container">
-
   <div class="row">
-
   	<div class="col-lg-12">
-
 		  <h3><span data-i18n="teamviewer.reporttitle"></span> <span id="total-count" class='label label-primary'>…</span></h3>
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
@@ -22,12 +19,17 @@ new Teamviewer_model;
 		        <th data-i18n="teamviewer.clientid" data-colname='teamviewer.clientid'></th>	        
 		        <th data-i18n="teamviewer.always_online" data-colname='teamviewer.always_online'></th>	        
 		        <th data-i18n="teamviewer.version" data-colname='teamviewer.version'></th>	        
+		        <th data-i18n="teamviewer.update_available" data-colname='teamviewer.update_available'></th>	        
+		        <th data-i18n="teamviewer.updateversion" data-colname='teamviewer.updateversion'></th>	        
+		        <th data-i18n="teamviewer.autoupdatemode" data-colname='teamviewer.autoupdatemode'></th>	        
+		        <th data-i18n="teamviewer.security_adminrights" data-colname='teamviewer.security_adminrights'></th>	        
+		        <th data-i18n="teamviewer.prefpath" data-colname='teamviewer.prefpath'></th>	        
 		      </tr>
 		    </thead>
 		    <tbody>
-		    	<tr>
-					<td data-i18n="listing.loading" colspan="3" class="dataTables_empty"></td>
-				</tr>
+		      <tr>
+		        <td data-i18n="listing.loading" colspan="11" class="dataTables_empty"></td>
+		      </tr>
 		    </tbody>
 		  </table>
     </div> <!-- /span 12 -->
@@ -47,16 +49,19 @@ new Teamviewer_model;
 	$(document).on('appReady', function(e, lang) {
 		// Get column names from data attribute
 		var columnDefs = [],
-            col = 0; // Column counter
+		col = 0; // Column counter
 		$('.table th').map(function(){
-              columnDefs.push({name: $(this).data('colname'), targets: col});
-              col++;
+	             columnDefs.push({name: $(this).data('colname'), targets: col});
+	             col++;
 		});
 	    oTable = $('.table').dataTable( {
 	        columnDefs: columnDefs,
 	        ajax: {
                 url: appUrl + '/datatables/data',
-                type: "POST"
+                type: "POST",
+                data: function(d){
+                     d.mrColNotEmpty = "clientid";
+                }
             },
             dom: mr.dt.buttonDom,
             buttons: mr.dt.buttons,
@@ -65,16 +70,34 @@ new Teamviewer_model;
 	        	var name=$('td:eq(0)', nRow).html();
 	        	if(name == ''){name = "No Name"};
 	        	var sn=$('td:eq(1)', nRow).html();
-	        	var link = mr.getClientDetailLink(name, sn);
+	        	var link = mr.getClientDetailLink(name, sn, '#tab_teamviewer-tab');
 	        	$('td:eq(0)', nRow).html(link);
+                
+	        	// Connect button
+	        	var colvar=$('td:eq(3)', nRow).html();
+	        	$('td:eq(3)', nRow).html(+colvar+'&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-info btn-xs" style="min-width: 80px;" href="teamviewer10://control?device='+colvar+'"  target="_blank">'+i18n.t('teamviewer.connect')+'</a>')
 	        	
 	        	// Status
 	        	var always_online=$('td:eq(4)', nRow).html();
-	        	always_online = always_online == 1 ? '<span class="label label-success">'+i18n.t('enabled')+'</span>' :
-	        	(always_online === '0' ? '<span class="label label-danger">'+i18n.t('disabled')+'</span>' : '')
+	        	always_online = always_online == 1 ? '<span class="label label-success">'+i18n.t('yes')+'</span>' :
+	        	(always_online === '0' ? '<span class="label label-danger">'+i18n.t('no')+'</span>' : '')
 	        	$('td:eq(4)', nRow).html(always_online)
-                
-	        }
+            
+	        	var colvar=$('td:eq(6)', nRow).html();
+	        	colvar = colvar == 1 ? '<span class="label label-success">'+i18n.t('yes')+'</span>' :
+	        	(colvar === '0' ? '<span class="label label-danger">'+i18n.t('no')+'</span>' : '')
+	        	$('td:eq(6)', nRow).html(colvar)
+            
+	        	var colvar=$('td:eq(8)', nRow).html();
+	        	colvar = colvar == 1 ? '<span class="label label-success">'+i18n.t('yes')+'</span>' :
+	        	(colvar === '0' ? '<span class="label label-danger">'+i18n.t('no')+'</span>' : '')
+	        	$('td:eq(8)', nRow).html(colvar)
+            
+	        	var colvar=$('td:eq(9)', nRow).html();
+	        	colvar = colvar == 1 ? '<span class="label label-success">'+i18n.t('yes')+'</span>' :
+	        	(colvar === '0' ? '<span class="label label-danger">'+i18n.t('no')+'</span>' : '')
+	        	$('td:eq(9)', nRow).html(colvar)
+       	        }
 	    });
 	});
 </script>
